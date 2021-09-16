b02f1534237fa4a5b18ee5f4d13d799668327f21
HaoGuosheng <2575459415@qq.com>
2021-09-16 17:33:11 +0800

    idea second commit

--- src/main/java/com/dz/git/GitTest.java
+++ src/main/java/com/dz/git/GitTest.java
@@ -3,5 +3,6 @@ package com.dz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test1");
+        System.out.println("Git Test1");
     }
 }
